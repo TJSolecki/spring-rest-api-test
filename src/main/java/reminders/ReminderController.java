a6c2f26692988b2f29c506f82137a442ec59e8e7
Thomas Solecki <thomasjsolecki@gmail.com>
2024-05-27 11:02:03 -0700

    Add GET /reminders/:index endpoint

--- src/main/java/reminders/ReminderController.java
+++ src/main/java/reminders/ReminderController.java
@@ -1,9 +1,16 @@
 package reminders;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequestMapping("reminders")
 public class ReminderController {
 
     private final ReminderRepository reminder_repository;
@@ -12,8 +19,17 @@ public class ReminderController {
         this.reminder_repository = reminder_repository;
     }
 
-    @GetMapping("/reminders")
-    String get_reminders() {
-        return reminder_repository.find_all().toString();
+    @GetMapping("/")
+    List<Reminder> get_reminders() {
+        return reminder_repository.find_all();
+    }
+
+    @GetMapping("/{i}")
+    Reminder get_reminder_by_index(@PathVariable int i) {
+        Optional<Reminder> reminder = reminder_repository.find_by_index(i);
+        if (reminder.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return reminder.get();
     }
 }
--- src/main/java/reminders/ReminderRepository.java
+++ src/main/java/reminders/ReminderRepository.java
@@ -4,13 +4,25 @@ import jakarta.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ReminderRepository {
     List<Reminder> reminders = new ArrayList<>();
 
-    public List<Reminder> find_all() { return reminders; }
+    List<Reminder> find_all() { return reminders; }
+
+    Optional<Reminder> find_by_index(int i) {
+        try {
+            Reminder reminder = reminders.get(i);
+            return Optional.of(reminder);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    void add_reminder(Reminder reminder) { reminders.add(reminder); }
 
     @PostConstruct
     private void init() {
