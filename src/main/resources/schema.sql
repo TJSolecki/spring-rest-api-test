bca5a94d862080e681df8433fece8e2136b9c65b
Thomas Solecki <thomasjsolecki@gmail.com>
2024-06-02 11:39:19 -0700

    Modify reminders table to work with Postgres

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS reminders (
-    id INT NOT NULL AUTO_INCREMENT,
+    id SERIAL NOT NULL,
     remind_date TIMESTAMP NOT NULL,
     message VARCHAR(256),
     email VARCHAR(256) NOT NULL,
