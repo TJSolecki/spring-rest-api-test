4d539b30bdaf6b52daa0458d484c6b233b7feae9
Thomas Solecki <thomasjsolecki@gmail.com>
2024-06-07 13:38:43 -0700

    Add error popup dialouge

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -3,7 +3,12 @@ const email_input = document.getElementById("email");
 const message_input = document.getElementById("message");
 const remind_date_input = document.getElementById("remind_date");
 const blur_background = document.querySelector(".blur-background");
-const close_popup = document.querySelector(".close-button");
+const success_close_popup_button = document.getElementById(
+    "close-success-button",
+);
+const success_popup = document.querySelector(".success-popup");
+const error_close_popup_button = document.getElementById("close-error-button");
+const error_popup = document.querySelector(".error-popup");
 
 if (submit_button) {
     addEventListener("submit", (e) => {
@@ -18,19 +23,34 @@ if (submit_button) {
         })
             .then((resp) => {
                 if (resp.status === 201) {
-                    blur_background.classList.remove("hidden");
+                    success_popup.classList.remove("hidden");
                 } else {
-                    alert("Failed to create reminder");
+                    error_popup.classList.remove("hidden");
                 }
             })
             .catch(() => {
-                alert("Failed to create reminder");
+                error_popup.classList.remove("hidden");
+            })
+            .finally(() => {
+                blur_background.classList.remove("hidden");
             });
     });
 }
 
-if (close_popup) {
-    close_popup.addEventListener("click", () => {
+if (success_close_popup_button) {
+    success_close_popup_button.addEventListener("click", () => {
         blur_background.classList.add("hidden");
+        success_popup.classList.add("hidden");
     });
+} else {
+    alert("Not here");
+}
+
+if (error_close_popup_button) {
+    error_close_popup_button.addEventListener("click", () => {
+        blur_background.classList.add("hidden");
+        error_popup.classList.add("hidden");
+    });
+} else {
+    alert("Not here");
 }
